4ed732ba15cfa9bcbbb8f8c5bc63d9f3ed04808e
MarkWasz <152340819+MarkWasz@users.noreply.github.com>
2023-12-04 19:12:45 -0500

    Update boardDOA.py
    Added the section to print the graph for Mic_tuning. Made sure I added some comments.

--- boardDOA.py
+++ boardDOA.py
@@ -3,6 +3,8 @@ from tuning import Tuning
 import usb.core
 import usb.util
 import time
+import numpy as np
+import matplotlib.pyplot as pyplot
 
 dev = usb.core.find(idVendor=0x2886, idProduct=0x0018)
 
@@ -12,6 +14,17 @@ if dev:
     while True:
         try:
             print (Mic_tuning.direction) #Prints angle of direction of sound
+            
+            #This Section Is For The Graph
+            theta = (float(Mic_tuning.direction)/180*np.pi) #Takes the Mic_tuning.direction and converts it to a usable theta value
+            radius = [1] #Radius is always 1 for visual aesthetics
+            fig1 = pyplot.figure()
+            Polar_Graph = fig1.add_subplot(111, projection='polar')
+            Polar_Graph.set_rticks([1])  # This Limits The Text On The Radius For Visual Aesthetics
+            Polar_Graph.scatter(theta,radius, c ='r') #Plots The Point
+            Polar_Graph.set_title("Mic_tuning.direction Location", va='bottom') #Adds Title Information
+            pyplot.show() #Shows the graph
+            
             time.sleep(1)
         except KeyboardInterrupt:
             break

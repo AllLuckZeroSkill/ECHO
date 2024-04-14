f399675f18bb528b44d23fe2959c88482fb040ce
AllLuckZeroSkill <94426606+AllLuckZeroSkill@users.noreply.github.com>
2024-04-14 16:18:08 -0400

    Update i2c_multiplexer.py

--- src/lib/i2c_multiplexer.py
+++ src/lib/i2c_multiplexer.py
@@ -26,45 +26,7 @@ class TCA9548:
         """Disables all channels."""
         self.bus.write_byte(self.address, 0x00)
 
-# Example usage
-if __name__ == "__main__":
-    mux = TCA9548()
-    
-    try:
-        # Select channel 3
-        mux.select_channel(2)
-        print("Channel 3 selected.")
-        # Add your I2C communication code here to communicate with devices on channel 3
-        hap = HapticMotorDriver()
-        hap.set_vibration(50)
-    except Exception as e:
-        print(e)
-    finally:
-        time.sleep(2)
-        hap.set_vibration(0)
-        # Disable all channels when done
-       # mux.disable_all_channels()
-        print("All channels disabled.")
 
-    try:
-        # Select channel 3
-        mux.select_channel(3)
-        print("Channel 3 selected.")
-        # Add your I2C communication code here to communicate with devices on channel 3
-        hap = HapticMotorDriver()
-        hap.set_vibration(50)
-    except Exception as e:
-        print(e)
-    finally:
-        time.sleep(2)
-        hap.set_vibration(0)
-        # Disable all channels when done
-       # mux.disable_all_channels()
-        print("All channels disabled.")
-        
-
-
-#
     
 
 
